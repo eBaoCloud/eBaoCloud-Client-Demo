3f711592f9be835b17edcf75e846fcef5e1903ae
leonluo <leonluo@LEONLUO7A32>
2017-04-24 14:16:28 +0800

    update project settings

--- com.ebao.gs.ebaocloud.sea.seg.cmi.sample/Login.cs
+++ com.ebao.gs.ebaocloud.sea.seg.cmi.sample/Login.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 // using ebaoCloud cmi client namespace
 using com.ebaocloud.client.thai.seg.cmi.api;
@@ -14,8 +10,8 @@ namespace com.ebao.gs.ebaocloud.sea.seg.cmi.sample
 {
     public class Login
     {
-        public static string sampleUserName = "SEG_TIB_01";
-        public static string samplePassword = "eBao1234";
+        public static string sampleUserName = "TIB_01";
+        public static string samplePassword = "Seg@1234";
         public void LoginAction()
         {
             PolicyService service = new PolicyServiceImpl();
